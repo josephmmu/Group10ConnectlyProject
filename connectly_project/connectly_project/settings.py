d0e7b39fa0764d11a75ef1addb651feb27774491
josephmmu <lr.jsales@mmdc.mcl.edu.ph>
2025-02-10 01:28:13 +0800

    added HTML In settings.py

--- connectly_project/connectly_project/settings.py
+++ connectly_project/connectly_project/settings.py
@@ -43,8 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-'rest_framework',
-'posts',
+    'rest_framework',
+    'posts',
 ]
 
 MIDDLEWARE = [
